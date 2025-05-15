ebf3ec234d484334516aea724d3c255fd98d2632
Jaimeemj <123424910+Jaimeemj@users.noreply.github.com>
2025-05-14 23:22:36 -0500

    CHAO PETIRRER y hola appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    //Aqui van los modules
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcial',
+      entities: [
+
+      ],
+      dropSchema: true, 
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
